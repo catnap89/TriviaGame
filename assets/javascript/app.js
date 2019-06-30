375ebbf5453fe2fe501aac29fcda6c6e56a51e9f
youngwoo.cho <petshop0907@gmail.com>
2019-06-29 22:53:12 -0400

    updated psuedocoding for javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,53 @@
+// Psuedo Coding 
+/*
+
+# When website is loaded
+  * Only the Title and Start button is shown
+
+# When Buttons are hovered (Don't forget to do this!)
+  * use :hover (in css)
+
+# When the Start button is clicked
+  * Hide or Replace Start button with 
+    * Time Remaining: -- Seconds
+    * Trivia Question
+    * 4 answer buttons
+
+# When one of the answer buttons are clicked
+  * If the answer is incorrect
+    * Time Remaining is still displayed but the timer stops.
+    * Trivia Question and answer buttons are hidden or replaced with
+      * Nope! (Incorrect Answer Message)
+      * The Correct Answer was: 
+      * GIF or Image related to the correct answer
+    * After few seconds, automatically shows next trivia question and reset time remaining and run the timer.
+  
+  * If the answer is correct
+    * Time remaining timer stops and is still displayed
+    *  Trivia Question and answer buttons are hidden or replaced with
+      * Correct!
+      * GIF or image related to the correct answer
+  
+  * If the answer is not selected and timer runs out
+    * Time Remaining stops at 0, it is displayed.
+    * Trivia Question and answer buttons are hidden or replaced with
+      * Out of Time! (Time's up message)
+      * Display correct answer
+      * GIF or image related to the correct answer
+    * After few seconds, automatically shows the next trivia question and reset time remaining and run the timer.
+
+# Aftrer last question
+  * Timer is still displayed. ( I believe it does not really matter if it is being displayed or not.)
+  * All done, here's how you did! (ending message) is displayed
+  * The counter with following features are displayed
+    * Correct answers:
+    * Incorrect answers:
+    * Unanswered: 
+  * Start Over? button is displayed
+
+# When Start Over button is clicked
+  * It basically resets and start the game again (Probably will have same or similar functionality with START button)
+  
+
+*/
+
